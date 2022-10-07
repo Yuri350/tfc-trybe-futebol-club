e85eea5cc8ed0308080ca9ce522666337210b73c
Yuri Villanueva <yuri.350@hotmail.com>
2022-10-07 19:01:06 -0300

    UserService, validando email e senha

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -7,18 +7,25 @@ interface StatusError extends Error {
   status: number,
 }
 
-const nome1 = (status: number, message: string) => {
+const catchError = (status: number, message: string) => {
   const error: StatusError = { message, status, name: 'error' };
   return error;
 };
 
 export default class UserService {
   public static async login(input: { email: string, password: string }): Promise<string> {
+    const Error4041 = 'All fields must be filled';
+    const Error4042 = 'Incorrect email or password';
+
     const result = await UserModel.findOne({ where: { email: input.email } });
-    if (result === null) throw nome1(400, 'Incorrect email or password');
+    if (result === null) throw catchError(400, Error4041);
 
     const validPass = Bcrypt.compare(input.password, result.password);
-    if (!validPass) throw nome1(400, 'Incorrect email or password');
+    if (!validPass) throw catchError(400, Error4041);
+
+    if (input.email !== result.email) throw catchError(401, Error4042);
+    if (input.password !== result.password) throw catchError(401, Error4042);
+
     const token = createToken(input);
     return token;
   }
